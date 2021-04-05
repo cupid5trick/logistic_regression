43b0f06c56c39927cb52cd88a558b9bd46fa18b0
cupid5trick <50273795+cupid5trick@users.noreply.github.com>
2021-04-05 22:05:35 +0800

    Init.

---
+++ logistic_regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class LogisticRegression(object):
+    computed = False
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
